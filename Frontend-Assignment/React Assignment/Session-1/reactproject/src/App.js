456937ef336e15de467f4c9e262452d18d9acbc8
JENIL1108 <jenilgajjar1108@gmail.com>
2026-06-29 16:39:40 +0530

    all

---
+++ Frontend-Assignment/React Assignment/Session-1/reactproject/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './componets/Home';
+
+function App() {
+  return (
+<>
+<h1>Welcome to My React Zomato App</h1>
+<Home/>
+</>
+  );
+}
+
+export default App;
---
+++ Frontend-Assignment/React Assignment/Session-1/reactproject/src/componets/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h2>Trending on Spotify: Calm Down</h2>
+    </div>
+  )
+}
+
+export default Home
